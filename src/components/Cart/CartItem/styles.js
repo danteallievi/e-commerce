e0f7c40102c8f56027b653b1cd91457f8384121a
Dante Allievi <toto99.-@hotmail.com>
2021-04-27 16:39:49 +0200

    First commit

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  media: {
+    height: 260,
+  },
+  emptyCart: {
+    textAlign: "center",
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  buttons: {
+    display: "flex",
+
+    alignItems: "center",
+  },
+}));
---
+++ src/components/CheckoutForm/FormInput.jsx
@@ -0,0 +1,19 @@
+import { useFormContext, Controller } from "react-hook-form";
+import { TextField, Grid } from "@material-ui/core";
+
+function FormInput({ name, label }) {
+  const { control } = useFormContext();
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        defaultValue=""
+        control={control}
+        name={name}
+        render={({ field }) => <TextField fullWidth label={label} required />}
+      />
+    </Grid>
+  );
+}
+
+export default FormInput;
